a0e04ee25f9ac288645bec7dce9f4f9fcbb14d07
danbortolato <daniel.bortolato@gmail.com>
2022-06-08 08:44:34 -0300

    improve-train01

--- train01.py
+++ train01.py
@@ -4,8 +4,8 @@ l = LinearRegression()
 Input = [[1], [2], [3], [4], [5]]
 Output = [[10], [20], [30], [40], [50]]
 l.train(Input, Output)
-enter = [[6]]
+enter = [[6], [7], [8]]
 result = l.regression(enter)
 print(f'{enter}: {result}')
 
-# training to convert input in *10 and float - Ex. 6 = 60.0
+# training improve with more values
